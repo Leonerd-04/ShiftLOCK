46495fb3c2c8160c66eafc7383a287eda6134ad1
leonard04 <leonerd04.2718@gmail.com>
2023-06-08 23:44:18 -0700

    Initial Commit

---
+++ src/main/java/me/ieonard04/shiftlock/ShiftLOCK.java
@@ -0,0 +1,12 @@
+package me.ieonard04.shiftlock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShiftLOCK implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
