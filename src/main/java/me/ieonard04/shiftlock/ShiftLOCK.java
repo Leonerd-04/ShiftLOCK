840097fe4152d55c6b1a5391ef1f9a0b477cffde
leonard04 <leonerd04.2718@gmail.com>
2023-06-10 17:53:37 -0700

    Add keybinding and mod id to main class

--- src/main/java/me/ieonard04/shiftlock/ShiftLOCK.java
+++ src/main/java/me/ieonard04/shiftlock/ShiftLOCK.java
@@ -1,12 +1,25 @@
 package me.ieonard04.shiftlock;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class ShiftLOCK implements ClientModInitializer {
 
+    public static KeyBinding SHIFT_LOCK;
+    public final static String MOD_ID = "shift_lock";
 
     @Override
     public void onInitializeClient() {
-
+        SHIFT_LOCK = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "existing_stacks.move",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_CAPS_LOCK, // Default keybinding is Caps Lock Key
+                        "existing_stacks.category"
+                )
+        );
     }
 }
