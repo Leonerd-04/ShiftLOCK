19e42090f6656e021c17855b391ea152af17b6ca
leonard04 <leonerd04.2718@gmail.com>
2023-08-08 17:10:41 -0700

    Get the config option to affect the mod

--- src/main/java/me/ieonard04/shiftlock/mixin/KeyboardInputMixin.java
+++ src/main/java/me/ieonard04/shiftlock/mixin/KeyboardInputMixin.java
@@ -4,15 +4,20 @@ import net.minecraft.client.input.Input;
 import net.minecraft.client.input.KeyboardInput;
 import org.spongepowered.asm.mixin.Mixin;
 
-import me.ieonard04.shiftlock.ShiftLOCK;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import me.ieonard04.shiftlock.ShiftLOCK;
+import me.ieonard04.shiftlock.ShiftLOCKConfig;
 
 @Mixin(KeyboardInput.class)
 public class KeyboardInputMixin extends Input {
     @Inject(method = "tick", at = @At("TAIL"))
     private void onTick(boolean slowDown, float slowDownFactor, CallbackInfo ci){
+        // Handles if not both are active, since XOR and OR are the same for this case
         this.sneaking ^= ShiftLOCK.IS_SHIFT_LOCKED;
+        // Handles if both are active
+        this.sneaking |= ShiftLOCKConfig.OR & (this.sneaking || ShiftLOCK.IS_SHIFT_LOCKED);
     }
 }
